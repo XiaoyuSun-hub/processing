1d6a040482d7b08285df5dbd27972d165a0c3254
x.sun <zhushusun@gmail.com>
2020-11-15 11:30:13 +0100

    test

--- Tilling.py
+++ Tilling.py
@@ -2,3 +2,4 @@ import os
 raster_tiler = sol.tile.raster_tile.RasterTiler(dest_dir='rio_chips',  # the directory to save images to
                                                 src_tile_size=(500, 500),  # the size of the output chips
                                                 verbose=True)
+test
